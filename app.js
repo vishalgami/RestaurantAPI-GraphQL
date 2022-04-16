d38208e0f6de9dc3674f1935b453bff17e6f847b
Vishal Gami <itsmevishal4@gmail.com>
2022-04-15 20:35:27 -0400

    New version

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.use('/graphql', graphqlHTTP({
     graphiql:true
 }));
 
+app.get('/',function (req, res) {
+    res.redirect('/graphql');
+});
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
