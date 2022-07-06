5096159306e68c2ca1d58378a8da4db1596174e9
Vishal Gami <itsmevishal4@gmail.com>
2022-07-05 20:46:28 -0400

    RestaurantAPI GraphQL

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+/******************************************************************************
+***
+* ITE5315 – Project
+* I declare that this assignment is my own work in accordance with Humber Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Group member Name: Balraj Singh - N01415998,Vishal Gami - N01452433 Date: 16-04-2022
+*
+*
+******************************************************************************
+**/
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./model/restaurantSchema')
